46eb2b4cca648e20d3134e0b686b2cf28c0eda27
Matias <matias_mateu@icloud.com>
2019-02-09 21:08:58 +0100

    Deploy

--- playlists/route.js
+++ playlists/route.js
@@ -2,7 +2,12 @@ const { Router } = require('express')
 const Playlist = require('./model')
 const bcrypt = require('bcryptjs');
 const User = require('../users/model')
+const Album = require('../albums/model')
+const Artist = require('../artists/model')
+const Song = require('../songs/model')
 const { toData } = require('../auth/jwt')
+const auth = require ('../auth/middleware')
+const PlaylistsSongs = require ('../playlists_songs/model')
 
 const router = new Router()
 
@@ -29,7 +34,7 @@ router.post('/playlists', (req, res, next) => {
   if (auth && auth[0] === 'Bearer' && auth[1]) {
     const user = toData(auth[1])
     Playlist
-      .findOrCreate({ where: { name: req.body.name },defaults: { user_id: user }})
+      .findOrCreate({ where: { name: req.body.name }, defaults: { user_id: user } })
       .spread(function (user, created) {
         if (created) {
           res.status(201).send({
@@ -54,65 +59,78 @@ router.post('/playlists', (req, res, next) => {
 
 router.get('/playlists/:id', (req, res, next) => {
   const auth = req.headers.authorization && req.headers.authorization.split(' ')
-  if (auth && auth[0]==='Bearer' && auth[1]){
+  if (auth && auth[0] === 'Bearer' && auth[1]) {
     const data = toData(auth[1])
     Playlist
-    .findAll({where:{user_id:data}})
-    .then(playlist => {
-      if (!playlist) {
-        return res.status(204).send({
-          message: `No content`
-        })
-      }else{
-        const pl = playlist.filter(pl=>pl['id']==req.params.id)
-        if (pl.length>0){
-          return res.status(200).send(pl[0])
-        }else{
-          return res.status(204).send("No content")
+      .findAll({ where: { user_id: data } })
+      .then(playlist => {
+        if (!playlist) {
+          return res.status(204).send({
+            message: `No content`
+          })
+        } else {
+          const pl = playlist.filter(pl => pl['id'] == req.params.id)
+          if (pl.length > 0) {
+            return res.status(200).send(pl[0])
+          } else {
+            return res.status(204).send("No content")
+          }
+
         }
-        
-      }
-    })
-    .catch(error => next(error))
-    
-  }else {
+      })
+      .catch(error => next(error))
+
+  } else {
     res.status(401).send({
       message: 'Please supply some valid credentials'
     })
   }
 })
 
-router.delete('/playlists/:id'),(req,res,next)=>{
+router.delete('/playlists/:id'), (req, res, next) => {
   const auth = req.headers.authorization && req.headers.authorization.split(' ')
-  if (auth && auth[0]==='Bearer' && auth[1]){
+  if (auth && auth[0] === 'Bearer' && auth[1]) {
     const user = toData(auth[1])
     Playlist
-    .findById(req.params.id)
-    .then(
-      playlist => {
-        if (playlist['user_id']==user){
-          Playlist.destroy({where:{id:req.params.id}})
-          res.status(200).send({
-            message: 'Playlist deleted'
-          })
-        }else{
-          res.status(400).send({
-            message: 'You cannot delete this playlist'
-          })
+      .findById(req.params.id)
+      .then(
+        playlist => {
+          if (playlist['user_id'] == user) {
+            Playlist.destroy({ where: { id: req.params.id } })
+            res.status(200).send({
+              message: 'Playlist deleted'
+            })
+          } else {
+            res.status(400).send({
+              message: 'You cannot delete this playlist'
+            })
+          }
         }
-      }
-    )
-    .catch(res.status(400).send("No results..."))
-  }else {
+      )
+      .catch(res.status(400).send("No results..."))
+  } else {
     res.status(401).send({
       message: 'Please supply some valid credentials'
     })
   }
-  
+
 }
 
+router.post('/playlists/:id/songs', (req, res) => {
+  const auth = req.headers.authorization && req.headers.authorization.split(' ')
+  if (auth && auth[0] === 'Bearer' && auth[1]) {
+    const user = toData(auth[1])
+    // FIND OR CREATE ARTIST
+
+    // FIND OR CREATE ALBUM
+
+    // FIND OR CREATE SONG
+
+    // CREATE RELATION SONG - PL in PlayListsSongs
+    }
+})
+
+
 Playlist.belongsTo(User, { foreignKey: 'user_id', sourceKey: 'id' });
-
-
 
 module.exports = router
